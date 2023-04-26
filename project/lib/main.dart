bc70c6be8d842fb327f68acd0a4dda1c5cdf7cf6
Yaroslav <job.yaroslav@list.ru>
2023-04-26 16:08:46 +0300

    MultiProvider implmenting for Cart feature

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -3,16 +3,23 @@ import './screens/products_oveview_screen.dart';
 import './screens/product_details_screen.dart';
 import 'providers/products_provider.dart';
 import 'package:provider/provider.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) =>
-          Products(), //Provider который предоставляет instance во все нижние классы, но обновляются только те которые "слушают" изменения provider в том числе и context
-
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        //Provider который предоставляет instance во все нижние классы, но обновляются только те которые "слушают" изменения provider в том числе и context
+        ChangeNotifierProvider(
+          create: (xtc) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(primarySwatch: Colors.blue, accentColor: Colors.green),
